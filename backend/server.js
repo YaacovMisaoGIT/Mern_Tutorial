33c5bd35417937f7baec949ac8d4aeaa9e7a819f
yaacovmisao747 <103446275+yaacovmisao747@users.noreply.github.com>
2023-01-04 23:59:40 +0200

    third commit

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,21 @@
 // require('dotenv').config()
 const dotenv = require('dotenv').config()
+const colors = require('colors')
+const { errorHandler} = require('./middleWare/errorMiddleware')
+const connectDB = require('./config/db')
 const port =  process.env.PORT || 8000 //either this port or this
+
+connectDB()
 
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
 
-app.use(express.json())  //allows us to use middle ware
+app.use(express.json())  //allows us to use middle ware //body parser for raw json
+app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server Started. Listening on port ${port}`))
 // app.listen(process.env.PORT, () => console.log('Server Started. Listening on port...'))
